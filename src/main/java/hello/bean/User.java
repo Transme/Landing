8f50e8535f953372d18dab73bc98706e03c47902
lyw <1505872625@qq.com>
2020-01-05 14:17:14 +0800

    create basic structure

--- src/main/java/hello/bean/User.java
+++ src/main/java/hello/bean/User.java
@@ -19,4 +19,12 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
